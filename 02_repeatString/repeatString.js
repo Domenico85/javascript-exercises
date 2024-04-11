fad3e431d59359f9dfd75627206ec0ae477745ff
Dome <arillotta@gmail.com>
2024-04-11 10:33:01 +0200

    exercice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (typeof string !== 'string' || num < 0){
+        return 'ERROR';
+    }else if(num === 0){
+        return '';
+    }
 
+    let finalString = string;
+    for ( let i=1; i<num; i++){
+        finalString += string;
+    }
+    return finalString;
 };
-
+// repeatString()
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const reversedString = str.split("").reduce((acc,char) => char + acc, "" ); 
+    return reversedString
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
