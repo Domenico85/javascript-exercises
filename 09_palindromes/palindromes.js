dcb1a57c90de552b57813c550c2cacb2459d3112
Dome <arillotta@gmail.com>
2024-04-13 16:25:13 +0200

    exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const lowerCaseStr = str.toLowerCase();
+    const punctuationStr = lowerCaseStr.replace(/[\W_]/g, '');
+    const reversedString = punctuationStr.split('').reverse().join('');
+if (str === reversedString){
+    return true;
+}else if(str !== punctuationStr){
+    return true;
+}else{
+    return false;
+}
 
 };
 
